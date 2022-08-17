1224dde46d372c5cb01f1551d0b6f29c5f0f7e32
Nehemiah Dias <microsoftdavid4@gmail.com>
2022-08-16 18:28:02 -0700

    add audio playback

--- src/components/drumMachineComponent/DrumMachineComponent.js
+++ src/components/drumMachineComponent/DrumMachineComponent.js
@@ -2,19 +2,33 @@ import React from 'react';
 import './DrumMachine.css';
 
 function DrumMachineComponent() {
+    const playSound = (e) => {
+        const audio = document.getElementById(e.target.name);
+        console.log(e.target.name);
+        audio.play();
+    }
     return (
         <main>
             <div id='board-background'>
                 <div className='sound-controls'>
-                    <button>Q</button>
-                    <button>W</button>
-                    <button>E</button>
-                    <button>A</button>
-                    <button>S</button>
-                    <button>D</button>
-                    <button>Z</button>
-                    <button>X</button>
-                    <button>C</button>
+                    <button name='q' onClick={playSound}>Q</button>
+                    <audio id='q' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'></audio>
+                    <button name='w' onClick={playSound}>W</button>
+                    <audio id='w' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'></audio>
+                    <button name='e' onClick={playSound}>E</button>
+                    <audio id='e' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'></audio>
+                    <button name='a' onClick={playSound}>A</button>
+                    <audio id='a' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'></audio>
+                    <button name='s' onClick={playSound}>S</button>
+                    <audio id='s' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'></audio>
+                    <button name='d' onClick={playSound}>D</button>
+                    <audio id='d' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'></audio>
+                    <button name='z' onClick={playSound}>Z</button>
+                    <audio id='z' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'></audio>
+                    <button name='x' onClick={playSound}>X</button>
+                    <audio id='x' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'></audio>
+                    <button name='c' onClick={playSound}>C</button>
+                    <audio id='c' className='clip' src='https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'></audio>
                 </div>
                 <div className='other-controls'>
                     <div className='power'>
